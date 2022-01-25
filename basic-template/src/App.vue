07a18cd671572f16047ea7f08eb9b96d28cf1b8c
vichuta <vichuta.nice@mail.kmutt.ac.th>
2022-01-25 10:17:05 +0700

    week2

---
+++ basic-template/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+const msg1="get text {{ }} Mustache"
+const msg2="get text by v-text"
+</script>
+ 
+<template>
+<h1>Hello, Vue.js</h1>
+<ul>
+  <li>{{msg1}}</li>
+  <li v-text="msg2"></li>
+</ul>
+</template>
+ 
+<style>
+</style>
